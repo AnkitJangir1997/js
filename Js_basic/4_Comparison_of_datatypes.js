2aa0d4b8840f3429f8d1d0d07547de259b960500
ANKIT JANGID <32219733+AnkitJangir1997@users.noreply.github.com>
2023-01-18 06:23:21 +0000

    comparison

---
+++ Js_basic/4_Comparison_of_datatypes.js
@@ -0,0 +1,25 @@
+// console.log("Ankit");
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// this type of comparison avoid 
+// this is confusing
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >=0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log("2" === 2);
